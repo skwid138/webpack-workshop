f88c5fc0024ea6f9742afc068dfa806b872900bf
Hunter Rancourt <skwid138@gmail.com>
2017-12-09 12:52:46 -0600

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 var webpackConfig = {
-	entry: "./src/index.js",
+	// entry can be a string for single entry point or an object for multiple entry points
+	// webpack builds out dependencies based on this file down
+	entry: "./src/index.js", 
+	// defines where the application file is placed after it's been bundled
+	// the file created is what will be used for production code
 	output: {
+		// where to put the build file
 		path: "build",
+		// what name to give the build file
 		filename: "bundle.js"
 	},
+	// this can contain many properties to customize webpack
 	module: {
+		// define how I want installed loaders to process info
 		loaders: [
 			{
 				loader: "babel-loader",
